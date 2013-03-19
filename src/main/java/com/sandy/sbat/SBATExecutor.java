5367f41fb9dbd254a3e6e5878f65e580c5593cb1
Sandeep <sandeep.rawat@jasperindia.com>
2013-03-19 09:32:19 +0530

    Added file execution functionality in SBATExecutor

--- src/main/java/com/sandy/sbat/SBATExecutor.java
+++ src/main/java/com/sandy/sbat/SBATExecutor.java
@@ -1,5 +1,10 @@
 package com.sandy.sbat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 import com.sandy.sbat.common.SBATRequest;
@@ -16,10 +21,38 @@ public class SBATExecutor {
 	public void execute(List<Operation> operations) {
 		
 		for (Operation operation : operations) {
-			SBATRequest sbatRequest = SBATRequestFactory.SINGLETON.getNewInstance(operation.getCommandString(), operation.getParametersString());
-			sbatRequest.getCommand().execute(sbatRequest);
+			executeOperation(operation);
 		}
 //		WebDriverExecutor.SINGLETON.getWebDriver().
+	}
+
+	private void executeOperation(Operation operation) {
+		SBATRequest sbatRequest = SBATRequestFactory.SINGLETON.getNewInstance(operation.getCommandString(), operation.getParametersString());
+		sbatRequest.getCommand().execute(sbatRequest);
+	}
+	
+	public void execute(File operationsFile) {
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(operationsFile));
+			String operationString;
+			try {
+				while ((operationString = br.readLine()) != null) {
+					Operation operation = new Operation(operationString);
+					executeOperation(operation);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
