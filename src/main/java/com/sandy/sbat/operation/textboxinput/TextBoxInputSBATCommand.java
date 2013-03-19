b48305f0ceb72651acdb40c8bcd6173c29bda215
Sandeep <sandeep.rawat@jasperindia.com>
2013-03-19 12:08:22 +0530

    Updated functionality of text box input, now the Text box input command checks the availability of text box on screen

--- src/main/java/com/sandy/sbat/operation/textboxinput/TextBoxInputSBATCommand.java
+++ src/main/java/com/sandy/sbat/operation/textboxinput/TextBoxInputSBATCommand.java
@@ -1,13 +1,27 @@
 package com.sandy.sbat.operation.textboxinput;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.sandy.sbat.common.SBATCommand;
 import com.sandy.sbat.common.SBATRequest;
 import com.sandy.sbat.common.SBATResponse;
+import com.sandy.sbat.common.WebDriverExecutor;
 
 public class TextBoxInputSBATCommand implements SBATCommand {
 
 	public SBATResponse execute(SBATRequest request) {
-		return null;
+		TextBoxInputSBATRequest textBoxInputSBATRequest = (TextBoxInputSBATRequest)request;
+
+		
+		WebDriver webDriver = WebDriverExecutor.SINGLETON.getWebDriver();
+		WebDriverWait webDriverWait = new WebDriverWait(webDriver, 300);
+		WebElement textBoxElement = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(textBoxInputSBATRequest.getTargetElementXPath())));
+		textBoxElement.sendKeys(textBoxInputSBATRequest.getText());
+		return new TextBoxInputSBATResponse();
 	}
 
 }
---
+++ src/main/java/com/sandy/sbat/operation/textboxinput/TextBoxInputSBATResponse.java
@@ -0,0 +1,7 @@
+package com.sandy.sbat.operation.textboxinput;
+
+import com.sandy.sbat.common.SBATResponse;
+
+public class TextBoxInputSBATResponse implements SBATResponse {
+
+}
