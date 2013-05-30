842b9ec74d5b178423cfae7943bf85e8425e51cc
Gaurav Kaushik <gaurav.kaushik@snapdeal.com>
2013-05-30 12:39:47 +0530

    Updated test script filepath

--- src/main/java/com/sandy/sbat/client/SBATFileExecutorClient.java
+++ src/main/java/com/sandy/sbat/client/SBATFileExecutorClient.java
@@ -1,5 +1,8 @@
 package com.sandy.sbat.client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import com.sandy.sbat.SBATExecutor;
@@ -8,8 +11,14 @@ public class SBATFileExecutorClient {
     
 	public static void main(String[] args) {
 		SBATExecutor sbatExecutor = new SBATExecutor();
-		ClassLoader loader=Thread.currentThread().getContextClassLoader();
-		InputStream in = loader.getResourceAsStream("buy_flow.txt");
+		String filePath=System.getProperty("user.dir")+"/TestScripts/buy_flow.txt";
+		File file = new File(filePath);
+		InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 		sbatExecutor.execute(in);
 	}
 	
--- src/main/java/com/sandy/sbat/operation/verifyurl/VerifyURLSBATCommand.java
+++ src/main/java/com/sandy/sbat/operation/verifyurl/VerifyURLSBATCommand.java
@@ -1,10 +1,6 @@
 package com.sandy.sbat.operation.verifyurl;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.sandy.sbat.common.SBATCommand;
 import com.sandy.sbat.common.SBATRequest;
